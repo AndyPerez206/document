d28e98c6421255cf93befb232557e06c63923785
andy2025 <andyblogger2015@gmail.com>
2025-08-15 20:50:26 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -31,6 +31,28 @@ function contactWhatsApp() {
     window.open("https://wa.me/51997531712", "_blank");
 }
 
+import { animate, stagger, text } from 'animejs';
+
+const { chars } = text.split('h2', { words: false, chars: true });
+
+animate(chars, {
+  // Property keyframes
+  y: [
+    { to: '-2.75rem', ease: 'outExpo', duration: 600 },
+    { to: 0, ease: 'outBounce', duration: 800, delay: 100 }
+  ],
+  // Property specific parameters
+  rotate: {
+    from: '-1turn',
+    delay: 0
+  },
+  delay: stagger(50),
+  ease: 'inOutCirc',
+  loopDelay: 1000,
+  loop: true
+});
+
+
 function clearNotes() {
     if (confirm("¿Estás seguro de que deseas borrar las notas?")) {
         document.getElementById("textNotes").value = "";
